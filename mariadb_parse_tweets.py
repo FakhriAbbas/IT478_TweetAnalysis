853b0e674cd47dd214e7d0aa94fefd81f4bced00
tuftedocelot <tuftedocelot@tuftedocelot.me>
2014-09-30 15:30:09 -0500

    finish import

--- mariadb_parse_tweets.py
+++ mariadb_parse_tweets.py
@@ -1,6 +1,7 @@
 import os
 import json
 from random import randint
+from random import choice
 import mysql.connector
 from mysql.connector import errorcode
 
@@ -8,13 +9,14 @@ conn = mysql.connector.connect(user='lucas', password='ohjustpoutthen',
         host='localhost', database='it478')
 
 tables = {}
-tables['tweets'] = ("create table tweets (user_id bigint not null, tweet_text varchar(165), date_tweeted datetime, retweet_count smallint, tweet_id bigint not null primary key, sentimentScore double)")
 
 tables['users'] = ("create table users (user_id bigint not null primary key, gender char(1), age datetime, zipcode int(5), city varchar(45))")
 
-tables['sentiment_staging_users'] = ("create table users (staging_user_id bigint not null primary key, gender char(1), age datetime, zipcode int(5), city varchar(45), status varchar(45))")
+tables['tweets'] = ("create table tweets (user_id bigint not null, tweet_text varchar(165), date_tweeted datetime, retweet_count smallint, tweet_id bigint not null primary key, sentimentScore double, constraint fk_tweets_user foreign key (user_id) references users (user_id))")
 
-tables['sentiment_staging_tweets'] = ("create table tweets (staging_user_id bigint not null, tweet_text varchar(165), date_tweeted datetime, retweet_count smallint, tweet_id bigint not null primary key, sentimentScore double, status varchar(45))")
+tables['sentiment_staging_users'] = ("create table sentiment_staging_users (staging_user_id bigint not null primary key, gender char(1), age datetime, zipcode int(5), city varchar(45), status varchar(45))")
+
+tables['sentiment_staging_tweets'] = ("create table sentiment_staging_tweets (staging_user_id bigint not null, tweet_text varchar(165), date_tweeted datetime, retweet_count smallint, tweet_id bigint not null primary key, sentimentScore double, status varchar(45), constraint fk_tweets_staging_user foreign key (staging_user_id) references sentiment_staging_users (staging_user_id))")
 
 cursor = conn.cursor()
 
@@ -30,10 +32,6 @@ for name, ddl in tables.items():
     else:
         print("OK")
 
-
-cursor.execute('alter table tweets (constraint foreign key (user_id) references users (user_id))')
-cursor.execute('alter table sentiment_staging_tweets foreign key staging_user_id references sentiment_staging_users(user_id)')
-
 def getUserIDs(path):
     user_list = os.listdir(path)
     return user_list
@@ -46,6 +44,28 @@ def setGender():
         gender = 'M'
     return gender
 
+def setAge():
+    month = str(randint(1, 12))
+    day = str(randint(1, 28))
+    year = str(randint(1979, 1996))
+    birthday = year+'-'+month+'-'+day
+    return birthday
+
+def setZip():
+    zips = {    '61701':'Bloomington',
+                '61702':'Bloomington',
+                '61704':'Bloomington',
+                '61705':'Bloomington',
+                '61709':'Bloomington',
+                '61710':'Bloomington',
+                '61791':'Bloomington',
+                '61799':'Bloomington',
+                '61761':'Normal',
+                '61790':'Normal'        }
+
+    randzip = choice(list(zips.keys()))
+    return [randzip, zips[randzip]]
+
 def processUsers(user_list):
     for user in user_list:
         with open('collected_tweets/'+user, 'r') as user_file:
@@ -54,7 +74,9 @@ def processUsers(user_list):
                 print('skipping user: empty tweets')
             else:
                 gender = setGender()
-                cursor.execute('insert into users (user_id, gender) values (%s,%s)', (user, gender))
+                rzip = setZip()
+                age = setAge()
+                cursor.execute('insert into users (user_id, gender, age, zipcode, city) values (%s,%s,%s,%s,%s)', (user, gender, age, rzip[0], rzip[1]))
                 for tweet in tweets:
                     row = (user, tweet['text'], str('0'), tweet['retweet_count'], tweet['id_str'] )
                     cursor.execute('insert into tweets (user_id, tweet_text, date_tweeted, retweet_count, tweet_id) values (%s, %s, %s, %s, %s)', row)
